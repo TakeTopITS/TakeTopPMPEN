c933be476f2bae723411429f58311dfa7569d15c
TakeTopITS <service@taketopits.com>
2025-05-11 19:59:56 +0800

    web

--- TakeTopServer/restore.bat
+++
@@ -1,5 +0,0 @@
-rem restoring database..... 
-set PGPASSWORD=zxckkllzly
-echo on
-..\pgsetup\postgresql\bin\pg_restore.exe -h 127.0.0.1 -p 5432 -U postgres -w -d taketopdecmpendb -v E:\TakeTopDECMPWinSolutionENCore\ENSource\Database\TakeTopAppDBBackup.bak
- rem restoring database sucessfully£¡ 
